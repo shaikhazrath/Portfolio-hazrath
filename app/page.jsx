cb32387c45755c36fa6f966460330c1a3058fbcb
shaikhazrath <shaikhazrathali123@gmail.com>
2023-10-25 19:12:53 +0530

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,24 @@
 "use client";
 import ProjectCard from "@/components/main/ProjectCard";
 import React, { useState } from "react";
-import { AiFillHtml5, AiFillInstagram, AiFillLinkedin, AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineEllipsis } from 'react-icons/ai';
+import {
+  AiFillHtml5,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+  AiOutlineEllipsis
+} from 'react-icons/ai';
 import { RiTwitterXFill } from 'react-icons/ri'
-import { BiLogoCss3, BiLogoDjango, BiLogoFigma, BiLogoJavascript, BiLogoMongodb, BiLogoPython, BiLogoReact } from 'react-icons/bi'
+import {
+  BiLogoCss3,
+  BiLogoDjango,
+  BiLogoFigma,
+  BiLogoJavascript,
+  BiLogoMongodb,
+  BiLogoPython,
+  BiLogoReact
+} from 'react-icons/bi'
 import { SiExpress } from 'react-icons/si'
 import { DiNodejs } from 'react-icons/di'
 import { Button } from "@/components/ui/button";
@@ -14,52 +29,49 @@ export default function Home() {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
   return (
     <main>
-      <nav className="flex justify-between px-10 pt-5 items-center">
-        <h1 className=" md:text-3xl  font-semibold uppercase">Sk hazrath
-          <div className=" h-1 w-24 bg-black"></div>
-        </h1>
+      <nav className="flex justify-between px-4 md:px-10 py-4 md:pt-5 items-center">
+        <h1 className="md:text-3xl font-semibold uppercase">Sk hazrath</h1>
+        <div className="h-1 w-24 bg-black hidden md:block"></div>
       </nav>
       {/* hero section */}
-      <section className="flex justify-around flex-col-reverse md:flex-row px-16 items-center h-screen">
-        <div className="md:w-1/2 line space-y-5">
-          <h1 className=" font-extrabold  md:text-7xl ">Hey,  i’m  Developer.</h1>
-          <p className=" md:w-3/4  text-textmain md:text-xl">Web Developer with a Passion for Pixels and a Curious AI Mind, crafting digital experiences and exploring the future of AI, one code at a time. 🌐🤖🚀</p>
-          <Button>My Resume</Button>
-        </div>
-        <img className="w-full md:w-1/2 lg:w-1/4 rounded-md" src="heroimage.jpeg" alt="Your Image" />
-      </section>
-      {/* projects  */}
-      <section className="px-10 pt-5">
-        <h1 className=" text-3xl  font-semibold uppercase">Projects
-          <div className=" h-1 w-24 bg-black"></div>
-        </h1>
-        <div className="m-10 grid grid-cols-1 md:grid-cols-4 gap-4 ">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+      <section className="px-4 md:px-16 py-4 md:pt-5">
+        <div className="flex flex-col md:flex-row items-center">
+          <img className="w-full md:w-1/2 lg:w-1/4 rounded-md mb-4 md:mb-0" src="heroimage.jpeg" alt="Your Image" />
+          <div className="md:w-1/2 md:pl-8 space-y-4">
+            <h1 className="font-extrabold text-4xl md:text-7xl">Hey, I'm a Developer.</h1>
+            <p className="text-textmain text-lg md:text-xl">Web Developer with a Passion for Pixels and a Curious AI Mind, crafting digital experiences and exploring the future of AI, one code at a time. 🌐🤖🚀</p>
+            <Button>My Resume</Button>
+          </div>
         </div>
       </section>
-      {/* about  */}
-      <section className="px-10 pt-5">
-        <h1 className=" text-3xl  font-semibold uppercase">About
-          <div className=" h-1 w-16 bg-black"></div>
-        </h1>
-        <p className="md:p-10 p-2 font-medium text-lg">
+      {/* projects */}
+      <section className="px-4 md:px-10 py-4 md:pt-5">
+        <h1 className="text-3xl font-semibold uppercase">Projects</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+      </section>
+      {/* about */}
+      <section className="px-4 md:px-10 py-4 md:pt-5">
+        <h1 className="text-3xl font-semibold uppercase">About</h1>
+        <p className="font-medium text-lg pt-5 pb-4">
           Welcome to my portfolio website! I'm Hazrath, a dynamic full-stack developer with extensive experience in Python (Django), JavaScript (React, React Native, Express, Node.js, and MongoDB), and Figma design.
           I have a passion for building high-performing applications, both large and small, and thrive on solving complex problems for my clients. As a tech enthusiast, I'm on an exciting journey of continuous learning, with a keen interest in the fascinating world of artificial intelligence (AI) and its potential to revolutionize the digital landscape.
           I stay at the forefront of the latest technologies, design trends, and AI developments, constantly expanding my knowledge and skillset to deliver cutting-edge solutions. In addition to my programming expertise, I'm also skilled in using Figma to create beautiful, intuitive designs that prioritize user experience.
           Please explore my portfolio to view my latest work and get in touch if you're interested in collaborating on a project, whether it's web development, mobile apps, or an AI-driven innovation. I look forward to hearing from you!
         </p>
-<div className="flex items-center gap-4">
-<h1 className=" text-lg font-semibold">Languages Frame-works and Tools i know
-        </h1>
-        <AiOutlineArrowRight/>
-</div>
-        <div className="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-15 gap-2 m-4">
+        <div className="flex items-center gap-4">
+          <h1 className="text-lg font-semibold">Languages Frameworks and Tools I Know</h1>
+          <AiOutlineArrowRight />
+        </div>
+        <div className="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-8 gap-2 pt-4 pb-4">
           <AiFillHtml5 size={40} />
           <BiLogoCss3 size={40} />
           <BiLogoJavascript size={40} />
@@ -72,8 +84,8 @@ export default function Home() {
           <BiLogoDjango size={40} />
         </div>
       </section>
-      <hr className="m-10" />
-      <h1 className=" font-light text-sm text-center p-4 ">Thank you for visiting my portfolio</h1>
+      <hr className="my-4 md:my-10" />
+      <h1 className="font-light text-sm text-center p-4">Thank you for visiting my portfolio</h1>
       <div className="fixed right-0 top-1/3 z-50 bg-transparent flex flex-col space-y-1">
         <AiOutlineEllipsis size={25} />
         <AiFillInstagram size={25} />
